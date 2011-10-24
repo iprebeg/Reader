0b50e564c92d21c832554e0e142a523c0267be23
iprebeg <ivor.prebeg@gmail.com>
2011-10-24 22:38:37 +0200

    removed statusbar

--- Classes/ReaderAppDelegate.m
+++ Classes/ReaderAppDelegate.m
@@ -39,7 +39,7 @@
 
 	mainWindow = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
 
-//	[[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationNone];
+	[[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationNone];
 
 	readerDemoController = [[ReaderDemoController alloc] initWithNibName:nil bundle:nil]; // Demo controller
 
