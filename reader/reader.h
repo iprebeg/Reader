ab6bd0bdd94d9956a40841ee163118a75fe8ebaf
Ivor Prebeg <Ivor.Prebeg@gmail.com>
2012-08-12 03:07:14 +0200

    added libreader static target

---
+++ reader/reader.h
@@ -0,0 +1,13 @@
+//
+//  reader.h
+//  reader
+//
+//  Created by Ivor Prebeg on 8/12/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface reader : NSObject
+
+@end
---
+++ reader/reader.m
@@ -0,0 +1,13 @@
+//
+//  reader.m
+//  reader
+//
+//  Created by Ivor Prebeg on 8/12/12.
+//
+//
+
+#import "reader.h"
+
+@implementation reader
+
+@end
